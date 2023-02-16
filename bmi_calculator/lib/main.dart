50af16db2286910fa6ff290afed6e8493cd5e314
edgarmatthew <edgarmatthew247@gmail.com>
2023-02-16 11:07:59 +0300

    male and female

--- bmi_calculator/lib/main.dart
+++ bmi_calculator/lib/main.dart
@@ -14,6 +14,8 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   double age = 1;
   int _index = 0;
+  bool male = true, female = false;
+  
   @override
   Widget build(BuildContext context) {
     Widget page;
@@ -26,6 +28,20 @@ class _MyAppState extends State<MyApp> {
         break;
       default:
         throw UnimplementedError('No page at $_index');
+    }
+
+    void _toggleGender(var gender) {
+      if (gender == 'male') {
+        setState(() {
+          male = true;
+          female = false;
+        });
+      } else if (gender == 'female') {
+        setState(() {
+          male = false;
+          female = true;
+        });
+      }
     }
 
     return MaterialApp(
@@ -73,8 +89,13 @@ class _MyAppState extends State<MyApp> {
                   Row(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                      BigCard(sign: Icons.male),
-                      BigCard(sign: Icons.female),
+                      GestureDetector(
+                        onTap: () => _toggleGender('male'),
+                        child: BigCard(sign: Icons.male, flag: male),
+                      ),
+                      GestureDetector(
+                          onTap: () => _toggleGender('female'),
+                          child: BigCard(sign: Icons.female, flag: female)),
                     ],
                   ),
                   SizedBox(
@@ -105,28 +126,27 @@ class _MyAppState extends State<MyApp> {
 
 class BigCard extends StatelessWidget {
   final IconData sign;
+  final bool flag;
 
-  const BigCard({super.key, required this.sign});
+  const BigCard({super.key, required this.sign, required this.flag});
 
   @override
   Widget build(BuildContext context) {
     IconData sign = this.sign;
     return Card(
+      color: flag ? Colors.grey[800] : Colors.grey,
       shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
       child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Container(
+          padding: const EdgeInsets.all(8.0),
+          child: Container(
             alignment: Alignment.center,
             height: 170,
             width: 170,
-            child: InkWell(
-              child: Icon(
-                sign,
-                size: 50,
-              ),
-              onTap: null,
-            )),
-      ),
+            child: Icon(
+              sign,
+              size: 50,
+            ),
+          )),
     );
   }
 }
@@ -140,46 +160,60 @@ class Imperial extends StatelessWidget {
 
     return Form(
         key: _formKey,
-        child: Column(
-          children: [
-            Row(
-              children: [
-                TextFormField(
-                  keyboardType: TextInputType.number,
-                  decoration: InputDecoration(labelText: 'ft'),
-                  validator: (value) {
-                    if (value!.isEmpty) {
-                      return 'Please enter a value';
-                    }
-                    return null;
-                  },
-                  onSaved: (value) {},
+        child: Expanded(
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: TextFormField(
+                        keyboardType: TextInputType.number,
+                        decoration: InputDecoration(labelText: 'ft'),
+                        validator: (value) {
+                          if (value!.isEmpty) {
+                            return 'Please enter a value';
+                          }
+                          return null;
+                        },
+                        onSaved: (value) {},
+                      ),
+                    ),
+                    Expanded(
+                      child: TextFormField(
+                        keyboardType: TextInputType.number,
+                        decoration: InputDecoration(labelText: 'In'),
+                        validator: (value) {
+                          if (value!.isEmpty) {
+                            return 'Please enter a value';
+                          }
+                          return null;
+                        },
+                        onSaved: (value) {},
+                      ),
+                    ),
+                  ],
                 ),
-                TextFormField(
-                  keyboardType: TextInputType.number,
-                  decoration: InputDecoration(labelText: 'In'),
-                  validator: (value) {
-                    if (value!.isEmpty) {
-                      return 'Please enter a value';
-                    }
-                    return null;
-                  },
-                  onSaved: (value) {},
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Expanded(
+                  child: TextFormField(
+                    keyboardType: TextInputType.number,
+                    decoration: InputDecoration(labelText: 'lbs'),
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return 'Please enter a value';
+                      }
+                      return null;
+                    },
+                    onSaved: (value) {},
+                  ),
                 ),
-              ],
-            ),
-            TextFormField(
-              keyboardType: TextInputType.number,
-              decoration: InputDecoration(labelText: 'lbs'),
-              validator: (value) {
-                if (value!.isEmpty) {
-                  return 'Please enter a value';
-                }
-                return null;
-              },
-              onSaved: (value) {},
-            ),
-          ],
+              ),
+            ],
+          ),
         ));
   }
 }
@@ -193,31 +227,34 @@ class Metric extends StatelessWidget {
 
     return Form(
         key: _formKey,
-        child: Column(
-          children: [
-            TextFormField(
-              keyboardType: TextInputType.number,
-              decoration: InputDecoration(labelText: 'cm'),
-              validator: (value) {
-                if (value!.isEmpty) {
-                  return 'Please enter a value';
-                }
-                return null;
-              },
-              onSaved: (value) {},
-            ),
-            TextFormField(
-              keyboardType: TextInputType.number,
-              decoration: InputDecoration(labelText: 'kg'),
-              validator: (value) {
-                if (value!.isEmpty) {
-                  return 'Please enter a value';
-                }
-                return null;
-              },
-              onSaved: (value) {},
-            ),
-          ],
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              TextFormField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(labelText: 'cm'),
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return 'Please enter a value';
+                  }
+                  return null;
+                },
+                onSaved: (value) {},
+              ),
+              TextFormField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(labelText: 'kg'),
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return 'Please enter a value';
+                  }
+                  return null;
+                },
+                onSaved: (value) {},
+              ),
+            ],
+          ),
         ));
   }
 }
