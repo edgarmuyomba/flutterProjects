cbf47dc63c37981fc5bc0f270c872d26059043b4
edgarmatthew <edgarmatthew247@gmail.com>
2023-02-25 08:38:24 +0300

    changed homepage layout

--- weather/lib/homePage.dart
+++ weather/lib/homePage.dart
@@ -12,17 +12,87 @@ class _homePageState extends State<homePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-          child: SingleChildScrollView(
-        scrollDirection: Axis.horizontal,
-        child: Row(
-          children: [
-            BigCard(location: 'Kampala'),
-            BigCard(location: 'Kampala'),
-            BigCard(location: 'Kampala'),
-            BigCard(location: 'Kampala'),
-            BigCard(location: 'Kampala'),
-          ],
-        ),
+          child: Column(
+        children: [
+          SizedBox(
+            height: 120,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(25.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Berlin,',
+                      style: TextStyle(
+                        fontSize: 30,
+                        fontWeight: FontWeight.w500,
+                        ),
+                    ),
+                    Text(
+                      'Germany',
+                      style: TextStyle(
+                        fontSize: 30,
+                        fontWeight: FontWeight.w500
+                        ),
+                    ),
+                    Text(
+                      '13C',
+                      style: TextStyle(
+                        fontSize: 45,
+                        fontWeight: FontWeight.bold
+                        ),
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Card(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(25),
+                ),
+                child: Container(
+                  alignment: Alignment.center,
+                  height: 45,
+                  width: 95,
+                  child: Text('6%',
+                  style: TextStyle(fontSize: 15, fontWeight: FontWeight.w500)),
+                ),
+              ),
+              Card(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(25),
+                ),
+                child: Container(
+                  alignment: Alignment.center,
+                  height: 45,
+                  width: 95,
+                  child: Text('90%',
+                  style: TextStyle(fontSize: 15, fontWeight: FontWeight.w500)),
+                ),
+              ),
+              Card(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(25),
+                ),
+                child: Container(
+                  alignment: Alignment.center,
+                  height: 45,
+                  width: 95,
+                  child: Text('19km/h',
+                  style: TextStyle(fontSize: 15, fontWeight: FontWeight.w500)),
+                ),
+              ),
+            ],
+          )
+        ],
       )),
     );
   }
@@ -39,9 +109,7 @@ class BigCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(25)
-      ),
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(25)),
       margin: EdgeInsets.all(7),
       child: Container(
         height: 200,
