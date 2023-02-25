ec3b6bb3af6a1aeb616b5f0874a25b2314388b67
edgarmatthew <edgarmatthew247@gmail.com>
2023-02-25 09:42:10 +0300

    finished homePage

--- weather/lib/homePage.dart
+++ weather/lib/homePage.dart
@@ -12,98 +12,413 @@ class _homePageState extends State<homePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-          child: Column(
-        children: [
-          SizedBox(
-            height: 120,
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(25.0),
-                child: Column(
+          child: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 120,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(25.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Berlin,',
+                        style: TextStyle(
+                          fontSize: 30,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                      Text(
+                        'Germany',
+                        style: TextStyle(
+                            fontSize: 30, fontWeight: FontWeight.w500),
+                      ),
+                      Text(
+                        '13C',
+                        style: TextStyle(
+                            fontSize: 45, fontWeight: FontWeight.bold),
+                      )
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Card(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(25),
+                  ),
+                  child: Container(
+                    alignment: Alignment.center,
+                    height: 45,
+                    width: 95,
+                    child: Text('6%',
+                        style: TextStyle(
+                            fontSize: 15, fontWeight: FontWeight.w500)),
+                  ),
+                ),
+                Card(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(25),
+                  ),
+                  child: Container(
+                    alignment: Alignment.center,
+                    height: 45,
+                    width: 95,
+                    child: Text('90%',
+                        style: TextStyle(
+                            fontSize: 15, fontWeight: FontWeight.w500)),
+                  ),
+                ),
+                Card(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(25),
+                  ),
+                  child: Container(
+                    alignment: Alignment.center,
+                    height: 45,
+                    width: 95,
+                    child: Text('19km/h',
+                        style: TextStyle(
+                            fontSize: 15, fontWeight: FontWeight.w500)),
+                  ),
+                ),
+              ],
+            ),
+            Padding(
+              padding: const EdgeInsets.all(25.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Text(
+                    "Today",
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
+                  )
+                ],
+              ),
+            ),
+            SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  children: [
+                    BigCard(temperature: '12C', time: '6am'),
+                    BigCard(temperature: '15C', time: '9am'),
+                    BigCard(temperature: '20C', time: '12pm'),
+                    BigCard(temperature: '25C', time: '3pm'),
+                    BigCard(temperature: '30C', time: '6pm'),
+                    BigCard(temperature: '35C', time: '9pm'),
+                  ],
+                )),
+            SizedBox(
+              height: 20,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 23, right: 23),
+              child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Text(
-                      'Berlin,',
-                      style: TextStyle(
-                        fontSize: 30,
-                        fontWeight: FontWeight.w500,
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Monday',
+                          style: TextStyle(
+                              fontSize: 20, fontWeight: FontWeight.w500),
                         ),
+                        SizedBox(
+                          height: 30,
+                        ),
+                        Text(
+                          'Tuesday',
+                          style: TextStyle(
+                              fontSize: 20, fontWeight: FontWeight.w500),
+                        ),
+                        SizedBox(
+                          height: 30,
+                        ),
+                        Text(
+                          'Wednesday',
+                          style: TextStyle(
+                              fontSize: 20, fontWeight: FontWeight.w500),
+                        ),
+                        SizedBox(
+                          height: 30,
+                        ),
+                        Text(
+                          'Thursday',
+                          style: TextStyle(
+                              fontSize: 20, fontWeight: FontWeight.w500),
+                        ),
+                        SizedBox(
+                          height: 30,
+                        ),
+                        Text(
+                          'Friday',
+                          style: TextStyle(
+                              fontSize: 20, fontWeight: FontWeight.w500),
+                        ),
+                        SizedBox(
+                          height: 30,
+                        ),
+                        Text(
+                          'Saturday',
+                          style: TextStyle(
+                              fontSize: 20, fontWeight: FontWeight.w500),
+                        ),
+                        SizedBox(
+                          height: 30,
+                        ),
+                        Text(
+                          'Sunday',
+                          style: TextStyle(
+                              fontSize: 20, fontWeight: FontWeight.w500),
+                        ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                      ],
                     ),
-                    Text(
-                      'Germany',
-                      style: TextStyle(
-                        fontSize: 30,
-                        fontWeight: FontWeight.w500
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Icon(Icons.cloud),
+                        SizedBox(
+                          height: 35,
                         ),
+                        Icon(Icons.cloud),
+                        SizedBox(
+                          height: 30,
+                        ),
+                        Icon(Icons.cloud),
+                        SizedBox(
+                          height: 35,
+                        ),
+                        Icon(Icons.cloud),
+                        SizedBox(
+                          height: 30,
+                        ),
+                        Icon(Icons.cloud),
+                        SizedBox(
+                          height: 35,
+                        ),
+                        Icon(Icons.cloud),
+                        SizedBox(
+                          height: 30,
+                        ),
+                        Icon(Icons.cloud),
+                        SizedBox(
+                          height: 20,
+                        ),
+                      ],
                     ),
-                    Text(
-                      '13C',
-                      style: TextStyle(
-                        fontSize: 45,
-                        fontWeight: FontWeight.bold
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: [
+                        SizedBox(
+                          height: 5,
                         ),
+                        Text(
+                          '13c',
+                          style: TextStyle(
+                              fontSize: 15, fontWeight: FontWeight.w500),
+                        ),
+                        SizedBox(
+                          height: 40,
+                        ),
+                        Text(
+                          '13c',
+                          style: TextStyle(
+                              fontSize: 15, fontWeight: FontWeight.w500),
+                        ),
+                        SizedBox(
+                          height: 35,
+                        ),
+                        Text(
+                          '13c',
+                          style: TextStyle(
+                              fontSize: 15, fontWeight: FontWeight.w500),
+                        ),
+                        SizedBox(
+                          height: 40,
+                        ),
+                        Text(
+                          '13c',
+                          style: TextStyle(
+                              fontSize: 15, fontWeight: FontWeight.w500),
+                        ),
+                        SizedBox(
+                          height: 35,
+                        ),
+                        Text(
+                          '13c',
+                          style: TextStyle(
+                              fontSize: 15, fontWeight: FontWeight.w500),
+                        ),
+                        SizedBox(
+                          height: 40,
+                        ),
+                        Text(
+                          '13c',
+                          style: TextStyle(
+                              fontSize: 15, fontWeight: FontWeight.w500),
+                        ),
+                        SizedBox(
+                          height: 35,
+                        ),
+                        Text(
+                          '13c',
+                          style: TextStyle(
+                              fontSize: 15, fontWeight: FontWeight.w500),
+                        ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                      ],
                     )
-                  ],
-                ),
-              ),
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              Card(
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(25),
-                ),
-                child: Container(
-                  alignment: Alignment.center,
-                  height: 45,
-                  width: 95,
-                  child: Text('6%',
-                  style: TextStyle(fontSize: 15, fontWeight: FontWeight.w500)),
-                ),
-              ),
-              Card(
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(25),
-                ),
-                child: Container(
-                  alignment: Alignment.center,
-                  height: 45,
-                  width: 95,
-                  child: Text('90%',
-                  style: TextStyle(fontSize: 15, fontWeight: FontWeight.w500)),
-                ),
-              ),
-              Card(
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(25),
-                ),
-                child: Container(
-                  alignment: Alignment.center,
-                  height: 45,
-                  width: 95,
-                  child: Text('19km/h',
-                  style: TextStyle(fontSize: 15, fontWeight: FontWeight.w500)),
-                ),
-              ),
-            ],
-          )
-        ],
+                  ]),
+            ),
+            // Padding(
+            //   padding: const EdgeInsets.only(left: 23, right: 23),
+            //   child: Row(
+            //     mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            //     children: [
+            //       Text(
+            //         'Monday',
+            //         style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
+            //       ),
+            //       Icon(Icons.cloud),
+            //       Text('13c', style: TextStyle(fontWeight: FontWeight.w500)),
+            //     ],
+            //   ),
+            // ),
+            // SizedBox(
+            //   height: 30,
+            // ),
+            // Padding(
+            //   padding: const EdgeInsets.only(left: 23, right: 23),
+            //   child: Row(
+            //     mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            //     children: [
+            //       Text(
+            //         'Tuesday',
+            //         style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
+            //       ),
+            //       Icon(Icons.cloud),
+            //       Text('17c', style: TextStyle(fontWeight: FontWeight.w500))
+            //     ],
+            //   ),
+            // ),
+            // SizedBox(
+            //   height: 30,
+            // ),
+            // Padding(
+            //   padding: const EdgeInsets.only(left: 23, right: 23),
+            //   child: Row(
+            //     mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            //     children: [
+            //       Text(
+            //         'Wednesday',
+            //         style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
+            //       ),
+            //       Icon(Icons.cloud),
+            //       Text('21c', style: TextStyle(fontWeight: FontWeight.w500))
+            //     ],
+            //   ),
+            // ),
+            // SizedBox(
+            //   height: 30,
+            // ),
+            // Padding(
+            //   padding: const EdgeInsets.only(left: 23, right: 23),
+            //   child: Row(
+            //     mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            //     children: [
+            //       Text(
+            //         'Thursday',
+            //         style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
+            //       ),
+            //       Icon(Icons.cloud),
+            //       Text('20c', style: TextStyle(fontWeight: FontWeight.w500))
+            //     ],
+            //   ),
+            // ),
+            // SizedBox(
+            //   height: 30,
+            // ),
+            // Padding(
+            //   padding: const EdgeInsets.only(left: 23, right: 23),
+            //   child: Row(
+            //     mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            //     children: [
+            //       Text(
+            //         'Friday',
+            //         style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
+            //       ),
+            //       Icon(Icons.cloud),
+            //       Text('16c', style: TextStyle(fontWeight: FontWeight.w500))
+            //     ],
+            //   ),
+            // ),
+            // SizedBox(
+            //   height: 30,
+            // ),
+            // Padding(
+            //   padding: const EdgeInsets.only(left: 23, right: 23),
+            //   child: Row(
+            //     mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            //     children: [
+            //       Text(
+            //         'Saturday',
+            //         style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
+            //       ),
+            //       Icon(Icons.cloud),
+            //       Text('13c', style: TextStyle(fontWeight: FontWeight.w500))
+            //     ],
+            //   ),
+            // ),
+            // SizedBox(
+            //   height: 30,
+            // ),
+            // Padding(
+            //   padding: const EdgeInsets.only(left: 23, right: 23),
+            //   child: Row(
+            //     mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            //     children: [
+            //       Text(
+            //         'Sunday',
+            //         style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
+            //       ),
+            //       Icon(Icons.cloud),
+            //       Text('18c', style: TextStyle(fontWeight: FontWeight.w500))
+            //     ],
+            //   ),
+            // ),
+            // SizedBox(
+            //   height: 30,
+            // ),
+          ],
+        ),
       )),
     );
   }
 }
 
 class BigCard extends StatelessWidget {
-  final String location;
+  final String temperature;
+  final String time;
 
   const BigCard({
     super.key,
-    required this.location,
+    required this.temperature,
+    required this.time,
   });
 
   @override
@@ -112,9 +427,19 @@ class BigCard extends StatelessWidget {
       shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(25)),
       margin: EdgeInsets.all(7),
       child: Container(
-        height: 200,
-        width: 200,
-        child: Text(location),
+        alignment: Alignment.center,
+        height: 100,
+        width: 100,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              temperature,
+              style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+            ),
+            Text(time)
+          ],
+        ),
       ),
     );
   }
