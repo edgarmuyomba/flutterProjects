b690a078a785dd29101dcfbba996a01da80a3dfd
edgarmatthew <edgarmatthew247@gmail.com>
2023-03-03 00:09:01 +0300

    getIngredients function

--- pantry_pal/one.dart
+++ pantry_pal/one.dart
@@ -1,22 +1,23 @@
-// spoonacular url - https://api.spoonacular.com/recipes/findByIngredients?ingredients=apples,+flour,+sugar&number=100&ranking=1&ignorePantry=false&apiKey=a22788893f79434f8852f9589d773ce2
+// receipe url - https://api.spoonacular.com/recipes/findByIngredients?ingredients=apples,+flour,+sugar&number=100&ranking=1&ignorePantry=false&apiKey=a22788893f79434f8852f9589d773ce2
+// ingredients url - https://api.spoonacular.com/recipes/1003464/ingredientWidget.json?apiKey=a22788893f79434f8852f9589d773ce2
 
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
-class receipts {
+class receipes {
   final int id;
   final String image;
   final int likes;
   final List missedIngredients;
 
-  const receipts(
+  const receipes(
       {required this.id,
       required this.image,
       required this.likes,
       required this.missedIngredients});
 
-  factory receipts.fromJson(List<dynamic> json) {
-    return receipts(
+  factory receipes.fromJson(List<dynamic> json) {
+    return receipes(
         id: json[0]['id'],
         image: json[0]['image'],
         likes: json[0]['likes'],
@@ -24,7 +25,37 @@ class receipts {
   }
 }
 
-Future<receipts> getReceipes(List ingredients) async {
+class ingredient {
+  final String name;
+  final double value;
+
+  const ingredient({
+    required this.name,
+    required this.value,
+  });
+
+  //temporary
+  @override
+  String toString() {
+    return 'Name: $name, Value: $value';
+  }
+}
+
+class ingredients {
+  final List details;
+
+  const ingredients({
+    required this.details,
+  });
+
+  factory ingredients.fromJson(Map<String, dynamic> json) {
+    return ingredients(
+      details: json['ingredients'],
+    );
+  }
+}
+
+Future<receipes> getReceipes(List ingredients) async {
   String attach = '';
   for (int i = 0; i < ingredients.length; i++) {
     if (i == ingredients.length - 1) {
@@ -38,7 +69,18 @@ Future<receipts> getReceipes(List ingredients) async {
           attach +
           '&number=100&ranking=1&ignorePantry=false&apiKey=a22788893f79434f8852f9589d773ce2'));
   if (response.statusCode == 200) {
-    return receipts.fromJson(jsonDecode(response.body));
+    return receipes.fromJson(jsonDecode(response.body));
+  } else {
+    throw Exception('Failed to load data');
+  }
+}
+
+Future<ingredients> getIngredients(int id) async {
+  final response = await http.get(Uri.parse('https://api.spoonacular.com/recipes/' +
+      id.toString() +
+      '1003464/ingredientWidget.json?apiKey=a22788893f79434f8852f9589d773ce2'));
+  if (response.statusCode == 200) {
+    return ingredients.fromJson(jsonDecode(response.body));
   } else {
     throw Exception('Failed to load data');
   }
@@ -46,10 +88,19 @@ Future<receipts> getReceipes(List ingredients) async {
 
 void main() {
   List sample = ['apples', 'cinnamon'];
-  Future<receipts> r1 = getReceipes(sample);
+  int id = 0;
+  Future<receipes> r1 = getReceipes(sample);
   r1.then((value) {
-    print(value.id);
-    print(value.likes);
-    print(value.image);
+    id = value.id;
+  });
+  Future<ingredients> I = getIngredients(id);
+  I.then((value) {
+    List _details = [];
+    List details = value.details;
+    for (Map detail in details) {
+      _details.add(ingredient(
+          name: detail['name'], value: detail['amount']['metric']['value']));
+    }
+    print(_details);
   });
 }
